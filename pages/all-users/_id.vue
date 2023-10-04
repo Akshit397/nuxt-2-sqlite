a473f44c9d8d5c19e3007849d72defb4a02e14a6
akshit <akshit@smartshore.nl>
2023-10-04 14:59:51 +0530

    kdns

--- pages/all-users/_id.vue
+++ pages/all-users/_id.vue
@@ -31,7 +31,7 @@ export default {
         // { property: "twitter:title", content: 'Overriden Title' },
       ]
       console.log('meta after timeout', this.meta)
-    }, 2000);
+    }, 10000);
   },
   head() {
     return {
